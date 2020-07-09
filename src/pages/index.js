f51b39400bd267fcdd9e4400c48db4cc5e047dd7
Amit Verma <amit.v@hotmail.com>
2020-07-08 20:14:44 -0400

    converted key logic to state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout';
 import Images from '../components/images/Images';
 
 import { Helmet } from 'react-helmet';
 
 const index = () => {
-  let unsplashClientKey;
+  const [key, setKey] = useState('');
 
-  if(process.env.NODE_ENV != 'production'){
-    unsplashClientKey = process.env.GATSBY_PORTFOLIO_ACCESS_KEY;
-  }
-  else{
-    unsplashClientKey = process.env.PORTFOLIO_ACCESS_KEY;
-  }
+  useEffect(() => {
+    if(process.env.NODE_ENV != 'production'){
+      setKey(process.env.GATSBY_PORTFOLIO_ACCESS_KEY);
+    }
+    else{
+      setKey(process.env.PORTFOLIO_ACCESS_KEY);
+    }
+  },[])
+
+  
 
   return (
     <Layout>
@@ -20,7 +24,7 @@ const index = () => {
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css" integrity="sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q" crossorigin="anonymous" />
       </Helmet>
       <div className="page">
-        <Images key={unsplashClientKey} />
+        <Images key={key} />
       </div>
     </Layout>
   )
